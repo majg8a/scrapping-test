abe20f46ecc1ca7f357cedaa5dcb5a0c4d58806a
majg8a <majg8a@gmail.com>
2023-09-21 20:42:40 -0400

    feat: element selector

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const puppeteer = require("puppeteer");
 const randomUseragent = require("random-useragent");
 
 const init = async () => {
-  const header = randomUseragent.getRandom();
+  const header = randomUseragent.getRandom(
+    (ua) => parseFloat(ua.browserVersion) >= 50 && ua.browserName === "Firefox"
+  );
   const browser = await puppeteer.launch({
     headless: true,
     executablePath: process.env.CHROME_BIN || null,
@@ -19,7 +21,17 @@ const init = async () => {
   await page.setViewport({ width: 1920, height: 1080 });
   const url = "https://www.youtube.com/";
   await page.goto(url);
-  await page.screenshot({ path: "step1.png" });
+  await page.screenshot({ path: `./images/${new Date().valueOf()}.png` });
+
+  await page.waitForSelector("#video-title-link");
+  const videoList = await page.$$("#video-title-link");
+  const videoUrls = [];
+  for (const item of videoList) {
+    const videoUrl = await page.evaluate((a) => a.getAttribute("href"), item);
+    videoUrls.push(`${url.slice(0,-1)}${videoUrl}`);
+  }
+  console.log(videoUrls);
+
   const onNavegationStopped = await browser.close();
 };
 init();
